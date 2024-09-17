36ddeaee7dda9c42323727b97bb26ec224a32d41
italloVidal <itallovidalll@gmail.com>
2024-09-17 15:45:44 -0300

    add prisma plugin to try to fix vercel deploy problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
+
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
 }
 
 export default nextConfig
